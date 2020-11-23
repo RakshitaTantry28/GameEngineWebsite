43096a92e4c2c9e4bc3c038c89e40034c0b9f38a
nidhivanjare <rnidhivanjare@gmail.com>
2020-11-23 13:56:44 +0530

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,119 @@
+<?php
+
+
+
+?>
+<head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Game World</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+       
+        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+ 
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+       
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+
+
+    <body id="page-top">
+        
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="" alt="" />Game World</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars ml-1"></i>
+                </button>
+              
+            </div>
+        </nav>
+        
+        <header class="masthead" >
+            <div class="container">
+          
+                <div class="masthead-heading text-uppercase" style="margin-top:-60px">Game World</div>
+                <a class=" btn-primary btn-xl text-uppercase js-scroll-trigger">DASHBOARD</a>
+            </div>
+        
+
+
+        <div id="dashboard" style="margin-top:60px">
+
+        	<center>
+			<table style="width:40% ; font-family: Lucida Console, Courier, monospace" class="text-uppercase" >
+
+			  <tr style="font-size:30px ; color:#4d79ff">
+			    <th >GAME</th>
+			    <th>USER_NAME</th> 
+			    <th>SCORE</th>
+			  </tr>
+
+			  <tr style="font-size:26px ; color:white">
+			    <td>Snake Game</td>
+			    <td></td> 
+			    <td></td>
+			  </tr>
+
+			  <tr style="font-size:26px; color:white">
+			    <td>Flappy Bird</td>
+			    <td></td> 
+			    <td></td>
+			  </tr>
+
+			   <tr style="font-size:26px; color:white">
+			   	<td>Quiz</td>
+			    <td></td> 
+			    <td></td>
+			  </tr>
+
+
+			<tr style="font-size:26px; color:white">
+			   <td>Goblin</td>
+			    <td></td> 
+			    <td></td>
+			  </tr>
+
+
+			<tr style="font-size:26px; color:white">
+			   <td>Space Shooter</td>
+			    <td></td> 
+			    <td></td>
+			  </tr>
+
+
+			  <tr style="font-size:26px; color:white">
+			   <td>Doodle Jump</td>
+			    <td></td> 
+			    <td></td>
+			  </tr>
+
+			  <tr style="font-size:26px; color:white">
+				<td>Guess Game</td>
+			    <td></td> 
+			    <td></td>
+			  </tr>
+
+			  <tr style="font-size:26px; color:white">
+				<td>Chess</td>
+			    <td></td> 
+			    <td></td>
+			  </tr>
+
+
+			</table>	
+
+			</center>
+        </div>
+
+        </header>
+   
+
+
+    </body>
+
