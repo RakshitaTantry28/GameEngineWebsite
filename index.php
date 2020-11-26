3067e59c948d8e5d6cd15e8b4ecd6d56e00f2a88
Rakshita <28rakshi@gmail.com>
2020-11-26 17:25:08 +0530

    Add

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ session_start();
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Game World</title>
+         <title>Game World</title> 
         <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
@@ -62,7 +62,7 @@ session_start();
                         <!-- <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#" class="text-danger"></li> -->
                     </ul>
                 </div>
-                <a class="navbar-brand js-scroll-trigger" href=""><img src="" alt="" style="margin-left: 20px" /><?php echo $_SESSION['indexuser'];?></a>
+                <a class="navbar-brand js-scroll-trigger" href=""><img src="img.jpg" height="5" alt="" style="margin-left: 30px; margin-right:7px" /><?php echo $_SESSION['indexuser'];?></a>
             </div>
         </nav>
 
@@ -72,7 +72,12 @@ session_start();
         <header class="masthead">
             <div class="container">
                 <div class="masthead-subheading">Game Set and Match !</div>
-                <div class="masthead-heading text-uppercase">Game World</div>
+                 <div class="masthead-heading text-uppercase">Game World</div> 
+                <!-- <p class="ml9"> 
+                    <span class="text-wrapper">
+                    <span class="letters">GAME WORLD </span>
+                    </span>
+                 </p> -->
                 <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#portfolio">Let's go</a>
             </div>
         </header>
@@ -837,6 +842,8 @@ session_start();
                 </div>
             </div>
         </div>
+        <script src="anime.min.js" ></script>
+        <script src="animateword.js" ></script>
         <!-- Bootstrap core JS-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
